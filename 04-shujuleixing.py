dca1bdf3974f4daa6a516aa4c4d403352db262d4
Yuweiluo1618 <yuwei.luo@alumni.ubc.ca>
2020-07-15 04:29:37 -0700

    7/14/20

---
+++ 04-shujuleixing.py
@@ -0,0 +1,22 @@
+"""
+Data type
+"""
+
+num1 = 1
+num2 = 1.1
+print(type(num1))
+
+a = 'hello world'
+print(type(a))
+
+c = [10, 20, 30]
+print(type(c))
+
+d = (10, 20, 30)
+print(type(d))
+
+e = {10, 20, 30}
+print(type(e))
+
+f = {'name': 'tom', 'age': 10}
+print(type(f))
---
+++ 05-FormatPrint.py
@@ -0,0 +1,7 @@
+age = 18
+name = 'TOM'
+weight = 75.5
+stu_id = 1
+print("age:%d" % age)
+print("name:%s" % name)
+print("weight:%.2f" % weight)
