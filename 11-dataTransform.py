371724db19bee21edb57f148f441aae6a1d875be
Yuweiluo1618 <yuwei.luo@alumni.ubc.ca>
2020-07-27 02:14:56 -0700

    '7/27/20'

---
+++ 11-dataTransform.py
@@ -0,0 +1,19 @@
+#num = input("number: ")
+#print(type(num))
+#print(type(int(num)))
+print(type(float(1)))
+print(float('1'))
+
+num1 = 1
+print(type(str(num1)))
+
+list1 = [1,2,3]
+print(type(tuple(list1)))
+
+t1 = (1,2,3)
+print(type(list(t1)))
+
+str1 = '1'
+str2 = '1.1'
+print(type(eval(str1)))
+print(type(eval(str2)))
