28a6b4118cc106f62c630ce1807350d154b6e2fa
Yuweiluo1618 <yuwei.luo@alumni.ubc.ca>
2020-09-21 01:42:45 -0700

    '9/21'

---
+++ 18-1 to 100 result.py
@@ -0,0 +1,7 @@
+result = 0
+count = 1
+while count <= 100:
+    result += count
+    count += 1
+
+print(result)
---
+++ 19-1 to 100  even 1.py
@@ -0,0 +1,18 @@
+count1 = 1
+result1 = 0
+
+while count1 < 101:
+    if count1 % 2 == 0:
+        result1 += count1
+    count1 += 1
+
+print(result1)
+
+count2 = 2
+result2 = 0
+
+while count2 < 101:
+        result2 += count2
+        count2 += 2
+
+print(result2)
