56cdb0ad73ce1e51c526c40ba107e554407c41b4
Yuweiluo1618 <yuwei.luo@alumni.ubc.ca>
2020-09-30 03:35:47 -0700

    '9/30/20'

---
+++ 29-set.py
@@ -0,0 +1,9 @@
+s1 = {10, 20, 30, 60, 70, 80, 90}
+s2 = set()
+print(s2)
+
+s1.add(100)
+print(s1.pop())
+
+print(10 in s1)
+print(10 not in s1)
---
+++ 31-list tui dao shi.py
@@ -0,0 +1,16 @@
+list1 = []
+
+i = 0
+while i < 10:
+    list1.append(i)
+    i += 1
+print(list1)
+
+list2 = []
+
+for i in range(10):
+    list2.append(i)
+print(list2)
+
+list3 = [i for i in range(10)]
+print(list3)
